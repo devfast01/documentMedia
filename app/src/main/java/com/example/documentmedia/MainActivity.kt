924e00ce94d2ba4fb8a567eb6aef4a153ce9926a
devfast01 <iot123tech@gmail.com>
2024-01-21 20:30:59 +0500

    exoPlayer2 notification is worked sucessfully

--- app/src/main/java/com/example/documentmedia/MainActivity.kt
+++ app/src/main/java/com/example/documentmedia/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.documentmedia
 
+import android.app.ActivityManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,23 +11,65 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        var isPlaying: Boolean = false
+        var musicService: MyService? = null
+
+    }
 
     lateinit var binding: ActivityMainBinding
     private val apiService = RetrofitService.getInstance()
+
+    private val serviceIntent by lazy {
+        Intent(this, MyService::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-        // Start MusicPlayerService with the audio URL
-            val serviceIntent = Intent(this, MyService::class.java)
+        binding.btnStart.setOnClickListener {
+            // Start the service when the "Start" button is clicked
+            startService()
+        }
+
+        binding.btnStop.setOnClickListener {
+            // Stop the service when the "Stop" button is clicked
+            stopService()
+        }
+
+    }
+
+    private fun startService() {
+        // Check if the service is already running
+        if (!isServiceRunning(MyService::class.java)) {
+            // Start the service with the audio URL
             serviceIntent.putExtra("audioUrl", "https://aydym.com/audioFiles/original/2023/07/11/07/34/b1b1282a-a4c5-485b-bd2c-a4d956d08a48.mp3")
             startService(serviceIntent)
         }
+        var isPlaying: Boolean = true
 
+    }
+
+    private fun stopService() {
+        // Stop the service
+        stopService(serviceIntent)
+        var isPlaying: Boolean = false
+
+    }
+
+    private fun isServiceRunning(serviceClass: Class<*>): Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if (serviceClass.name == service.service.className) {
+                return true
+            }
+        }
+        return false
     }
 
     fun getNewSongs() {
--- app/src/main/java/com/example/documentmedia/MyService.kt
+++ app/src/main/java/com/example/documentmedia/MyService.kt
@@ -1,17 +1,70 @@
 package com.example.documentmedia
 
+import android.R.attr.bitmap
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Binder
 import android.os.IBinder
+import android.support.v4.media.session.MediaSessionCompat
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
 
-class MyService: Service() {
+
+@Suppress("DEPRECATION")
+class MyService : Service() {
     private lateinit var exoPlayer: SimpleExoPlayer
+    private var myBinder = MyBinder()
+    private var mediaSession: MediaSessionCompat? = null
+
+    private lateinit var playerNotificationManager: PlayerNotificationManager
+    private lateinit var trackSelector: DefaultTrackSelector
+    private var notificationId = 123
+    private var channelId = "channelId"
+    private var UrlLink = ""
 
     override fun onCreate() {
         super.onCreate()
-        exoPlayer = SimpleExoPlayer.Builder(this).build()
+
+        trackSelector = DefaultTrackSelector(this)
+        exoPlayer = SimpleExoPlayer.Builder(this)
+            .setTrackSelector(trackSelector)
+            .build()
+        trackSelector.setParameters(
+            trackSelector
+                .buildUponParameters()
+                .setMaxVideoBitrate(0)
+        )
+
+    }
+
+    /**
+     * This class will be what is returned when an activity binds to this service.
+     * The activity will also use this to know what it can get from our service to know
+     * about the video playback.
+     */
+    inner class VideoServiceBinder : Binder() {
+
+        /**
+         * This method should be used only for setting the exoplayer instance.
+         * If exoplayer's internal are altered or accessed we can not guarantee
+         * things will work correctly.
+         */
+        fun getExoPlayerInstance() = exoPlayer
+        fun getService(): MyService = this@MyService
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        mediaSession = MediaSessionCompat(baseContext, "Music")
+        return myBinder
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -19,23 +72,126 @@ class MyService: Service() {
         if (audioUrl != null) {
             playAudio(audioUrl)
         }
-
         return START_NOT_STICKY
     }
 
     private fun playAudio(audioUrl: String) {
+        val context = this
         val mediaItem = MediaItem.fromUri(audioUrl)
         exoPlayer.setMediaItem(mediaItem)
         exoPlayer.prepare()
         exoPlayer.play()
+
+        exoPlayer.addListener(object : Player.EventListener {
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                if (!isPlaying) {
+                    stopForeground( /* removeNotification= */false)
+                }
+            }
+        })
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.music_cover_placeholder)
+
+        playerNotificationManager = PlayerNotificationManager.createWithNotificationChannel(
+            this,
+            channelId,
+            R.string.app_name,
+            R.string.app_name,
+            notificationId,
+            object : PlayerNotificationManager.MediaDescriptionAdapter {
+
+                override fun createCurrentContentIntent(player: Player): PendingIntent? {
+                    // return pending intent
+                    val intent = Intent(context, MainActivity::class.java)
+                    return PendingIntent.getActivity(
+                        context, 0, intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                    )
+                }
+
+                //pass description here
+                override fun getCurrentContentText(player: Player): String? {
+                    return "ExoPlayer PIP example"
+                }
+
+                //pass title (mostly playing audio name)
+                override fun getCurrentContentTitle(player: Player): String {
+                    return "ExoPlayer PIP example Video"
+                }
+
+                // pass image as bitmap
+                override fun getCurrentLargeIcon(
+                    player: Player,
+                    callback: PlayerNotificationManager.BitmapCallback,
+                ): Bitmap? {
+                    return bitmap
+                }
+            },
+            object : PlayerNotificationManager.NotificationListener {
+                override fun onNotificationPosted(
+                    notificationId: Int,
+                    notification: Notification,
+                    onGoing: Boolean,
+                ) {
+                    startForeground(notificationId, notification)
+                }
+
+                override fun onNotificationCancelled(
+                    notificationId: Int,
+                    dismissedByUser: Boolean,
+                ) {
+                    super.onNotificationCancelled(notificationId, dismissedByUser)
+                    stopSelf()
+                }
+            }
+        )
+
+        //attach player to playerNotificationManager
+        playerNotificationManager.setPlayer(exoPlayer) // this line brings the player to the notification bar
+        playerNotificationManager.setUseStopAction(true)
+        playerNotificationManager.setFastForwardIncrementMs(0)
+        playerNotificationManager.setRewindIncrementMs(0)
+        playerNotificationManager.setSmallIcon(R.drawable.music_cover_placeholder)
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
+//    val playerNotificationManagerBuilder =
+//        PlayerNotificationManager.Builder(this, 12, "CHANNEL_ID_12")
+//    playerNotificationManagerBuilder.setMediaDescriptionAdapter(mediaDescriptorInstance)
+//    val playerNotificationManager = playerNotificationManagerBuilder.build()
+//
+//    playerNotificationManager.setPlayer(exoPlayer)
+//
+//    private var mediaDescriptorInstance: MediaDescriptionAdapter = object : MediaDescriptionAdapter {
+//        override fun getCurrentContentTitle(player: Player): CharSequence {
+//            return "<Media Main Title>"
+//        }
+//
+//        override fun createCurrentContentIntent(player: Player): PendingIntent? {
+//            return null
+//        }
+//
+//        override fun getCurrentContentText(player: Player): CharSequence? {
+//            return "<Media sub-Title>"
+//        }
+//
+//        override fun getCurrentLargeIcon(player: Player, callback: BitmapCallback): Bitmap? {
+//            // Icon to display
+//            return null
+////            return bitmap
+//        }
+//    }
+
+
+    inner class MyBinder : Binder() {
+        fun currentService(): MyService {
+            return this@MyService
+        }
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
         exoPlayer.release()
     }
+
 }
